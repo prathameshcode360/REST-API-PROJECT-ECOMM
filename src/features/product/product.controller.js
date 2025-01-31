a6da587cc8690c3e4fe705bdc2d154eefdc6a895
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-01-31 13:07:47 +0530

    added file upload middleware

--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -13,7 +13,8 @@ export default class ProductController {
   }
   addProduct(req, res) {
     try {
-      const { name, price, image, category } = req.body;
+      const { name, price, category } = req.body;
+      const image = req.file.filename;
       const newProduct = ProductModel.add(name, price, image, category);
       return res.status(201).send({
         msg: "new product added",
--- src/features/product/product.routes.js
+++ src/features/product/product.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
 import ProductController from "./product.controller.js";
+import fileUpload from "../../middlewares/fileupload.middleware.js";
 const productRouter = express.Router();
 const productController = new ProductController();
 
 productRouter.get("/", productController.getProducts);
-productRouter.post("/add", productController.addProduct);
+productRouter.post(
+  "/add",
+  fileUpload.single("image"),
+  productController.addProduct
+);
 export default productRouter;
