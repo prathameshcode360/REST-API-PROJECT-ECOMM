650d5e0778887b782d9ea4e98b76e98f402dca7f
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-02-19 13:47:55 +0530

    created indexes

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,12 +1,14 @@
 import { MongoClient } from "mongodb";
 
+const url = "mongodb://localhost:27017/ecomdb";
 let client;
-console.log(process.env.DB_URL);
 export function connectToMongoDB() {
-  MongoClient.connect(process.env.DB_URL)
+  MongoClient.connect(url)
     .then((clientInstance) => {
       client = clientInstance;
       console.log("Mongo db connectd sucessfully");
+      createIndexes(client.db());
+      console.log("Indexes Crated sucessfully");
     })
     .catch((err) => {
       console.error(err);
@@ -15,4 +17,13 @@ export function connectToMongoDB() {
 
 export function getDb() {
   return client.db();
+}
+async function createIndexes(db) {
+  try {
+    await db.collection("products").createIndex({ price: 1 }); //single index
+    await db.collection("products").createIndex({ name: 1, category: -1 }); //compound index
+    await db.collection("products").createIndex({ desc: "text" }); //Text index
+  } catch (err) {
+    console.log("Something went wrong while creating the indexes", err);
+  }
 }
